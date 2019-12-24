a45414b6465feb9e88fc84d8b685dd33e3bc888c
chenb0x <artofmorellc@gmail.com>
2019-12-24 10:15:29 -0900

    Initial thoughts

---
+++ chenslator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+
+ScotsDict = {I: "A",
+    dont: "dinnae",
+    havent: "hivvnae",
+    yes: "aye",
+    no: "nay"
+}
+
+phrase = input("Enter a phrase to translate: ")
+print(phrase)
